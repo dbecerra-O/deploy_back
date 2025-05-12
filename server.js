af9adc26ef7616dd9d6a739ba491082e7c19f7b8
dbecerra-O <dbecerraorellana@gmail.com>
2025-05-12 02:16:19 -0500

    update back v2

--- server.js
+++ server.js
@@ -10,15 +10,8 @@ import detalleOrdenCompraRoutes from "./app/routes/detalleOrdenCompra.routes.js"
 
 const app = express();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "Password establecida" : "Password no establecida");
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_SSL:", process.env.DB_SSL);
-
 const corsOptions = {
-    origin: "http://localhost:5173", // Origen del frontend
+    origin: "*", // Permite cualquier origen
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true // Para permitir cookies/tokens
